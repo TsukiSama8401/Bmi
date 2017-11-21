9c46d1e3d8159dfeff24125a6dabacc4a2f787a5
yohanei <sleepcat10235269@gmial.com>
2017-11-21 12:04:09 +0800

    callback

--- app/src/main/java/com/hu/bmi6/MainActivity.java
+++ app/src/main/java/com/hu/bmi6/MainActivity.java
@@ -1,5 +1,6 @@
 package com.hu.bmi6;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -10,12 +11,16 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     String s = new String("abc");
+    private EditText weight;
+    private EditText height;
+    private Button bHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button bHelp = (Button) findViewById(R.id.b_info);
+        Log.d("MainActivity" , "onCreate");
+        bHelp = (Button) findViewById(R.id.b_info);
         bHelp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,21 +31,64 @@ public class MainActivity extends AppCompatActivity {
                         .show();
             }
         });
+        findView();
+    }
+
+    public MainActivity() {
+        super();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity" , "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity" , "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity" , "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MainActivity" , "onPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MainActivity" , "onResume");
+    }
+
+    private void findView() {
+        weight = (EditText) findViewById(R.id.ed_weight);
+        height = (EditText) findViewById(R.id.ed_height);
     }
 
     public void bmi (View view){
         Log.d("MainActivity" , "testing bmi method");
-        EditText weight = (EditText) findViewById(R.id.ed_weight);
-        EditText height = (EditText) findViewById(R.id.ed_height);
+
         float w = Float.parseFloat(weight.getText().toString());
         float h  =Float.parseFloat(height.getText().toString());
-        if (h >=3){
+
+        Intent intent = new Intent(this , ResultActivity.class);
+        intent.putExtra("BMI_EXTRA" , R.string.bmi);
+        startActivity(intent);
+
+        if (h >= 3){
             new AlertDialog.Builder(this)
                     .setMessage(R.string.error)
                     .setTitle(R.string.bmi)
                     .setPositiveButton(R.string.ok , null)
                     .show();
-
         }else{
             float bmi = w / (h * h);
             if (bmi < 20){
@@ -57,9 +105,5 @@ public class MainActivity extends AppCompatActivity {
                         .show();
             }
         }
-
-
     }
-
-
 }
---
+++ app/src/main/java/com/hu/bmi6/ResultActivity.java
@@ -0,0 +1,14 @@
+package com.hu.bmi6;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        float bmi = getIntent().getFloatExtra("BMI_EXTRA" , 0);
+    }
+}
